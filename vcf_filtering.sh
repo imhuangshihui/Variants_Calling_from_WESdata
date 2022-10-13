b5b97e6724fdb497e181e15894c0b5b23860174c
June HUANG <76728625+imhuangshihui@users.noreply.github.com>
2022-10-13 14:40:16 +0800

    Update vcf_filtering.sh

--- vcf_filtering.sh
+++ vcf_filtering.sh
@@ -1,14 +1,41 @@
+## Choose the exonic, UTR, and splicing region
+
+cat MR01.hg38_multianno.txt | awk -v OFS="\t" '$6=="exonic"{print}' > MR01.hg38_multianno.exonic
+cat MR01.hg38_multianno.txt | awk -v OFS="\t" '$6=="exonic;splicing"{print}' > MR01.hg38_multianno.exonicANDsplicing
+cat MR01.hg38_multianno.txt | awk -v OFS="\t" '$6=="splicing"{print}' > MR01.hg38_multianno.splicing
+cat MR01.hg38_multianno.txt | awk -v OFS="\t" '{if($6~"UTR")print}' > MR01.hg38_multianno.UTR
+cat h MR01.hg38_multianno.exonic MR01.hg38_multianno.exonicANDsplicing MR01.hg38_multianno.splicing && 
+MR01.hg38_multianno.splicing MR01.hg38_multianno.UTR > MR01.hg38_multianno.filtered
+
+# AWK uses the white space to splite the fields. "Nonsynonymous SNV" was seperated to 2 fields by AWK but as 1 field by CUT.
+
+## Filter by score
+
+# MR01.hg38_multianno.filtered 78956
+
 # SIFT < 0.05
-cat MR01.hg38_multianno.filterd | awk '{if($22<=0.05)print}' 
+cat MR01.hg38_multianno.filtered | awk -F '\t' '{if($21<=0.05)print}'
 
+# PolyPhen 2 HDIV: Probably damaging (>=0.957) 
+awk -F '\t' '{if($23>=0.957)print}'
 
-####以下開始要重新過濾，因爲awk提取出來的第23行和cut不一樣
-# PolyPhen 2 HDIV: Probably damaging (>=0.957) NUMBER: 1794
-awk '{if($23>=0.957)print}'
+# PolyPhen 2 HVar: Probably damaging (>=0.909) 
+awk -F '\t' '{if($25>0.909)print}'
 
-# PolyPhen 2 HVar: Probably damaging (>=0.909) NUMBER:1659
-awk '{if($25>=0.909)print}'
+# LRT is deleterious
+awk -F '\t' '$28!="U"{print}' | awk -F '\t' '$28!="N"{print}'
 
-# LRT is deleterious or neutral, only 2 variants was selected
-#awk '$28=="D"||$28=="N"{print}'
+# MutationAssessor (ma) choose high or medium. H/M means functional 
+awk -F '\t' '$32!="L"{print}' | awk -F '\t' '$32!="N"{print}'
+
+# FATHMM (fathmm) Deleterious
+awk -F '\t' '$34!="T"{print}'
+
+# MetaSVM (metasvm) D: Deleterious
+awk -F '\t' '$44!="T"{print}'
+
+# MetaLR (metalr) D: Deleterious
+awk -F '\t' '$46!="T"{print}'
+
+>MR01.hg38_multianno.score_filter  Number:39
 
