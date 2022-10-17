70c3b7d21326c1fa21809973bb8c64aacdcc313a
June HUANG <76728625+imhuangshihui@users.noreply.github.com>
2022-10-17 11:34:15 +0800

    Update vcf_filtering.sh

--- vcf_filtering.sh
+++ vcf_filtering.sh
@@ -7,7 +7,7 @@ cat MR01.hg38_multianno.txt | awk -v OFS="\t" '{if($6~"UTR")print}' > MR01.hg38_
 cat h MR01.hg38_multianno.exonic MR01.hg38_multianno.exonicANDsplicing MR01.hg38_multianno.splicing && 
 MR01.hg38_multianno.splicing MR01.hg38_multianno.UTR > MR01.hg38_multianno.filtered
 
-# AWK uses the white space to splite the fields. "Nonsynonymous SNV" was seperated to 2 fields by AWK but as 1 field by CUT.
+# （1）AWK uses the white space to splite the fields. "Nonsynonymous SNV" was seperated to 2 fields by AWK but as 1 field by CUT.
 
 ## Filter by score
 
@@ -39,6 +39,47 @@ awk -F '\t' '$46!="T"{print}' >MR01.hg38_multianno.score_filter
 
 #####Using all kinds of score to filter the vcf, the final variant number is 39.#####
 
+#(2) Extract the exonic variants first, then use score to filter the nonsynomos
+cat MR01.hg38_multianno.filtered | awk -F '\t' '{if($9!="synonymous SNV")print}' | awk -F '\t' '{if($9!="unknown")print}' | cut -f9 | sort | uniq -c
+  53177 .
+      1 ExonicFunc.refGene
+    145 frameshift deletion
+     99 frameshift insertion
+    229 nonframeshift deletion
+    185 nonframeshift insertion
+  12105 nonsynonymous SNV
+     31 startloss
+    129 stopgain
+     15 stoploss
+
+cat MR01.hg38_multianno.exonicfuntion | wc -l
+66116
+
+cat MR01.hg38_multianno.exonicfuntion | awk -F '\t' '{if($21<=0.05)print $21}' | wc -l
+57327
+
+awk -F '\t' '{if($23>=0.957)print}' | wc -l
+820
+
+awk -F '\t' '{if($25>0.909)print}' | wc -l
+541
+
+awk -F '\t' '$28!="U"{print}' | awk -F '\t' '$28!="N"{print}'
+372
+
+awk -F '\t' '$32!="L"{print}' | awk -F '\t' '$32!="N"{print}'
+275
+
+awk -F '\t' '$34!="T"{print}'
+70
+
+awk -F '\t' '$44!="T"{print}'
+38
+
+awk -F '\t' '$46!="T"{print}'
+37
+
+#Same results as the (1) method
 
 
 ## 用rs編號去NCBI Clinvar看這些變異是否有人報道過
