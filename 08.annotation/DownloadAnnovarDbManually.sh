dc82b4d5ea637492d39083b4c931c3f6553c0baa
June HUANG <juneyisen@gmail.com>
2023-06-20 11:28:59 +0800

    Create DownloadAnnovarDbManually.sh

---
+++ 08.annotation/DownloadAnnovarDbManually.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+perl  /mnt/hwstor9k_data1/junehuang/biosoft/annovar/annotate_variation.pl --downdb --buildver hg38  -downdb -webfrom annovar clinvar_20220320 /mnt/hwstor9k_data1/junehuang/biosoft/annovar/humandb/hg38
